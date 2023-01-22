9065b46b9f158c36f692cd3d3e96d692bcf01f01
Levan Kopaliani <ciceron23x@gmail.com>
2023-01-23 01:10:33 +0400

    homepage firs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,17 @@ const Home = () => {
   return (
     <main className="homepage">
       <NavBar />
+      <section className="section-one wrapper">
+        <h1 className="section-one__title">
+          Hypertriglyceridemia is a marker of increased risk
+          <span> for stroke, myocardial infarction.</span>
+        </h1>
+        <p className="section-one__subtitle">
+          Hypertriglyceridemia is a marker of increased risk for stroke,
+          myocardial infarction, diabetes, hypertension, and acute pancreatitis.
+        </p>
+        <button className="section-one__button">Learn more</button>
+      </section>
     </main>
   );
 };
